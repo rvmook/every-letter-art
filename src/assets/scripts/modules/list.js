150c3a8c3c5d39041a58eae18a7b69c984bfcfca
Rick van Mook <rick@rvmook.com>
2016-05-12 00:08:17 -0400

    Prepared Letter class

--- src/assets/scripts/modules/list.js
+++ src/assets/scripts/modules/list.js
@@ -2,35 +2,24 @@ var LETTER_G = 'M3,3.9h2.9v1.7c0,0.9-0.3,1.6-0.9,2.1C4.5,8.4,3.8,8.7,3,8.7c-0.7,
 
 var SVGGraphics = require('pixi-svg-graphics');
 
-var SVG_G = '<svg version="1.1" id="svg_g" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"width="5.9px" height="8.7px" viewBox="0 0 5.9 8.7" enable-background="new 0 0 5.9 8.7" xml:space="preserve"><path fill="red" d="M3,3.9h2.9v1.7c0,0.9-0.3,1.6-0.9,2.1C4.5,8.4,3.8,8.7,3,8.7c-0.7,0-1.2-0.2-1.6-0.5c-0.4-0.3-0.8-0.6-1-1C0.3,7,0.2,6.9,0.2,6.8c0-0.1-0.1-0.3-0.1-0.5C0,6,0,5.3,0,4.3c0-1,0-1.7,0.1-2C0.1,2,0.2,1.7,0.4,1.5c0.2-0.4,0.5-0.7,1-1.1C1.8,0.2,2.3,0,3,0c0.8,0,1.4,0.3,2,0.7c0.5,0.5,0.8,1.1,1,1.8H4.5C4.4,2.2,4.2,1.9,4,1.6C3.7,1.4,3.4,1.3,3,1.3c-0.3,0-0.5,0.1-0.8,0.2C2,1.6,1.8,1.7,1.7,1.9C1.5,2.1,1.4,2.3,1.3,2.6C1.3,2.9,1.3,3.5,1.3,4.3s0,1.4,0.1,1.7c0.1,0.3,0.2,0.5,0.3,0.7C1.8,6.9,2,7.1,2.2,7.2C2.4,7.3,2.7,7.4,3,7.4c0.5,0,0.9-0.2,1.2-0.5c0.3-0.3,0.5-0.8,0.5-1.3V5.1H3V3.9z"/></svg> ';
+var SVG_G = '<svg><path fill="red" d="M3,3.9h2.9v1.7c0,0.9-0.3,1.6-0.9,2.1C4.5,8.4,3.8,8.7,3,8.7c-0.7,0-1.2-0.2-1.6-0.5c-0.4-0.3-0.8-0.6-1-1C0.3,7,0.2,6.9,0.2,6.8c0-0.1-0.1-0.3-0.1-0.5C0,6,0,5.3,0,4.3c0-1,0-1.7,0.1-2C0.1,2,0.2,1.7,0.4,1.5c0.2-0.4,0.5-0.7,1-1.1C1.8,0.2,2.3,0,3,0c0.8,0,1.4,0.3,2,0.7c0.5,0.5,0.8,1.1,1,1.8H4.5C4.4,2.2,4.2,1.9,4,1.6C3.7,1.4,3.4,1.3,3,1.3c-0.3,0-0.5,0.1-0.8,0.2C2,1.6,1.8,1.7,1.7,1.9C1.5,2.1,1.4,2.3,1.3,2.6C1.3,2.9,1.3,3.5,1.3,4.3s0,1.4,0.1,1.7c0.1,0.3,0.2,0.5,0.3,0.7C1.8,6.9,2,7.1,2.2,7.2C2.4,7.3,2.7,7.4,3,7.4c0.5,0,0.9-0.2,1.2-0.5c0.3-0.3,0.5-0.8,0.5-1.3V5.1H3V3.9z"></path></svg>';
 
 var PIXI = require('../libs/pixi');
 var _win = window,
 	_doc = document,
+	_letter = new Letter(),
 	_container,
 	_canvasEl,
-	_graphics,
-	_renderer,
-	_isActive,
-	_image,
-	_context;
+	_renderer;
 
 
 init();
 
 function init() {
 
-	var div = _doc.createElement('div');
-
-	div.innerHTML = SVG_G;
-
-	var svgEl = div.querySelector('#svg_g');
-
 	_canvasEl = _doc.querySelector('.js-list');
 
 	if(_canvasEl) {
-
-		_isActive = false;
 
 		// create an new instance of a pixi stage
 		_container = new PIXI.Sprite();
@@ -42,6 +31,52 @@ function init() {
 
 		_renderer.backgroundColor = 0xffffff;
 
+		_letter.init(SVG_G);
+
+
+		_win.addEventListener('resize', onResize);
+		requestAnimationFrame(redraw);
+
+		onResize();
+	}
+}
+
+function onResize() {
+
+	_renderer.resize(_win.innerWidth, _win.innerHeight);
+
+	var newX = Math.round(_win.innerHeight / 2),
+		newY = Math.round(_win.innerWidth / 2);
+
+	_letter.updatePosition(newX, newY);
+}
+
+
+
+
+function redraw() {
+
+	requestAnimationFrame(redraw);
+
+	// render the stage
+	_renderer.render(_container);
+}
+
+function Letter() {
+
+	var _isFocused,
+		_image,
+		_graphics,
+		_pos = {x:0,y:0};
+
+	function init(svgString) {
+
+		var div = _doc.createElement('div'),
+			svgEl;
+
+		div.innerHTML = svgString;
+
+		svgEl = div.querySelector('svg');
 
 		_image = PIXI.Sprite.fromImage('assets/images/test.jpg');
 
@@ -57,57 +92,75 @@ function init() {
 		_graphics.scale.y = 10;
 
 		_image.mask = _graphics;
-
-		_win.addEventListener('resize', onResize);
-		_graphics.click = onCircleClick;
-		requestAnimationFrame(redraw);
-
-		onResize();
-	}
-}
-
-function onCircleClick() {
-
-	var scale;
-
-	_isActive = !_isActive;
-
-	if(_isActive) {
-
-		scale = 20;
-
-	} else {
-
-		scale = 10;
+		_graphics.click = onClick;
 	}
 
-	TweenLite.to(_graphics.scale, 0.35, {
-		x:scale,
-		y:scale,
-		onUpdate:onResize
-	})
-}
+	function onClick() {
 
-function onResize() {
+		if(_isFocused) {
 
-	_renderer.resize(_win.innerWidth, _win.innerHeight);
-	_graphics.y = _image.y = Math.round(_win.innerHeight / 2);
-	_graphics.x = _image.x = Math.round(_win.innerWidth / 2);
+			blur();
 
-	_graphics.x -= 3 * _graphics.scale.x;
-	_graphics.y -= 4.5 * _graphics.scale.y;
+		} else {
 
-	_image.x -= 230;
-	_image.y -= 230;
-}
+			focus();
+		}
+	}
 
+	function focus() {
 
+		_isFocused = true;
+		updateScale();
+	}
 
+	function blur() {
 
-function redraw() {
+		_isFocused = false;
+		updateScale();
+	}
 
-	requestAnimationFrame(redraw);
+	function updateScale() {
 
-	// render the stage
-	_renderer.render(_container);
+		var scale;
+
+		if(_isFocused) {
+
+			scale = 20;
+
+		} else {
+
+			scale = 10;
+		}
+
+		TweenLite.to(_graphics.scale, 0.35, {
+			x:scale,
+			y:scale,
+			onUpdate:redraw
+		})
+	}
+
+	function updatePosition(newX, newY) {
+
+		_pos.x = newX;
+		_pos.y = newY;
+
+		redraw();
+	}
+
+	function redraw() {
+
+		_graphics.y = _image.y = _pos.x;
+		_graphics.x = _image.x = _pos.y;
+
+		_image.x -= 230;
+		_image.y -= 230;
+
+		_graphics.x -= 3 * _graphics.scale.x;
+		_graphics.y -= 4.5 * _graphics.scale.y;
+	}
+
+	this.init = init;
+	this.focus = focus;
+	this.blur = blur;
+	this.updatePosition = updatePosition;
 }
